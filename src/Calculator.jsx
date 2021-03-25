40bd8e977a611e9e5c638d446eec8dc45db03949
Craig Gallimore <c.gallimore.it@gmail.com>
2021-03-25 12:54:57 +0200

    Improved overall code, making more reusable.

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,17 +1,98 @@
 import React, { useState } from 'react'
 
 function Calculator () {
-  const [total, setTotal] = useState('')
-  const [textInput, setTextInput] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [textInput, setTextInput] = useState('')
+  const [storedOperator, setStoredOperator] = useState('')
+  const buttonsValues = [
+    '1', '2', '3', '+',
+    '4', '5', '6', '-',
+    '7', '8', '9', '*',
+    '0', '00', '000', '/',
+  ]
+  let buttons = []
+
+  function onClickHandler(event) {
+    let valueInput = event.target.innerText
+    if (valueInput <= 9 && valueInput >= 0) {
+      {
+        setTextInput(textInput + valueInput)
+      }
+    }
+    else {
+      if (total != 0) {
+        switch (storedOperator) {
+          case '+':
+            setTotal(total + parseFloat(textInput))
+            setTextInput('')
+            break;
+          case '-':
+            setTotal(total - parseFloat(textInput))
+            setTextInput('')
+            break;
+          case '*':
+            setTotal(total * parseFloat(textInput))
+            setTextInput('')
+            break;
+          case '/':
+            setTotal(total / parseFloat(textInput))
+            setTextInput('')
+            break;
+          case '00':
+            setTextInput(textInput + valueInput)
+            break;
+          case '000':
+            setTextInput(textInput + valueInput)
+            break;
+
+          default:
+            break;
+        }
+      }
+      else {
+          setTotal(total + parseFloat(textInput))
+          setTextInput('')
+      }
+      setStoredOperator(valueInput)
+    }
+  }
+
+  for (let i = 0; i < buttonsValues.length; i++) {
+    buttons.push(
+      <button
+        key={buttonsValues[i]}
+        onClick={onClickHandler}>
+        {buttonsValues[i]}
+      </button>
+    )
+  }
+
   return (
     <div className='Calculator'>
       <h3>{total}</h3>
-      <input type="number" value={textInput} onChange={(event) => { setTextInput(event.target.value) }}></input><br></br>
-      <button onClick={() => { setTotal(+total + +textInput); setTextInput('') }}>Plus</button>
-      <button onClick={() => { setTotal(total - textInput); setTextInput('') }}>Minus</button>
-      <button onClick={() => { setTotal(total * textInput); setTextInput('') }}>Multiple</button>
-      <button onClick={() => { setTotal(total / textInput); setTextInput('') }}>Divide</button>
-      <button onClick={() => { setTotal(0); setTextInput('') }}>Clear</button>
+      <input 
+        type="number" 
+        value={textInput} 
+        onChange={(event) => { 
+          setTextInput(event.target.value) 
+        }}
+      />
+      <br>
+      </br>
+      {buttons}
+      <button
+        onClick={
+          onClickHandler
+        }>
+        =
+      </button>
+      <button
+        onClick={() => {
+          setTotal(0)
+          setTextInput('')
+        }}>
+        Clear
+      </button>
     </div>
   )
 }
